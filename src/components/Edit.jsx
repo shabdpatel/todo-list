4cf879507197026a9a31b67542e11b7cdabd4062
shabd patel <shabdpatel0@gmail.com>
2024-06-04 16:25:52 +0530

    done with project

--- src/components/Edit.jsx
+++ src/components/Edit.jsx
@@ -1,10 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-const Edit = () => {
+const Edit = ({ editTodo, task }) => {
+    const [value, setValue] = useState('')
+
+    const handleSubmit = (e) => {
+        e.preventDefault()  // Page does not reload
+        editTodo(value, task.id)
+        setValue('')
+    }
+
     return (
-        <div>
-
-        </div>
+        <form className='mb-4 font-primary w-full' onSubmit={handleSubmit}>
+            <input type='text' className='outline-none bg-transparent border border-gray-500 p-4 w-[300px] text-white mb-8 rounded placeholder:text-gray-100' placeholder="Update Task" onChange={(e) => setValue(e.target.value)} value={value} />
+            <button className='bg-gray-700 border-none p-2 text-white cursor-pointer rounded ml-2'>
+                Update Task
+            </button>
+        </form>
     )
 }
 
--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
+import { AiFillEdit } from 'react-icons/ai'
+import { BsFillTrashFill } from 'react-icons/bs'
 
-const Todo = () => {
+
+const Todo = ({ task, deleteTodo, editTodo }) => {
     return (
-        <div>
-
+        <div className='flex justify-between items-center bg-gray-700 text-white py-3 px-4 rounded-md mb-1 cursor-pointer'>
+            <p className='font-primary'>{task.task}</p>
+            <div className='flex items-center gap-x-4'>
+                <AiFillEdit className='text-xl' onClick={() => editTodo(task.id)} />
+                <BsFillTrashFill className='text-xl' onClick={() => deleteTodo(task.id)} />
+            </div>
         </div>
     )
 }
